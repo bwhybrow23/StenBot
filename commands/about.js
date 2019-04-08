c12c0ecc2023d50ea96dddee16738c57681d1349
Ben Whybrow <14111080+bwhybrow23@users.noreply.github.com>
2019-04-08 15:02:18 +0100

    Changed some stuff

--- commands/about.js
+++ commands/about.js
@@ -10,7 +10,7 @@ exports.run = (bot, message, args) => {
         embed: {
             title: 'About StenBot',
             color: bot.settings.green,
-            description: '\n**StenBot** is a multi-purpose bot designed to make your life easier and your server better. KewlBot is coded in Java and uses Mongo to store your precious data.\n\nYou can find out a lot more in our docs!\n[**https://docs.stenbot.net/**]'
+            description: '\n**StenBot** is a multi-purpose bot designed to make your life easier and your server better. StenBot is coded in JavaScript and uses the Discord.JS library to make coding easier.\n\nYou can find out a lot more in our docs!\n[**https://docs.stenbot.co.uk/**]'
         }
     });
 
