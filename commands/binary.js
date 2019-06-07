4a7ed05e73c7f9d92699da3559d4f78e5d3fd4f6
Ben Whybrow <14111080+bwhybrow23@users.noreply.github.com>
2019-06-07 23:03:16 +0100

    Reinstalled node modules

---
+++ commands/binary.js
@@ -0,0 +1,22 @@
+exports.run = async (bot, message, args) => {
+
+    var output = "";
+    const Discord = require("discord.js");
+
+    //HELP EMBED HERE
+
+    var input = args.join(" ");
+    for (var i = 0; i < input.length; i++) {
+        output += input[i].charCodeAt(0).toString(2) + " ";
+    }
+
+    const outputEmbed = new Discord.RichEmbed()
+    .setColor(bot.settings.yellow)
+    .setTitle("Binary Command")
+    .addField("Original Text", input)
+    .addField("Binary", output)
+    .setFooter(message.author.tag, message.author.displayURL);
+
+    message.channel.send(outputEmbed);
+
+}
