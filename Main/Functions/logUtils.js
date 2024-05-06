5cbb8b4692bf480b2ee9d2bc9c988be5522dd632
Ben Whybrow <ben@benwhybrow.com>
2024-05-06 10:21:22 +0100

    Added error handling and fixed upgrade to ES6 for logging

--- Main/Functions/logUtils.js
+++ Main/Functions/logUtils.js
@@ -3,6 +3,10 @@ import path from 'path';
 import * as fs from 'fs';
 import moment from 'moment';
 
+//__dirname
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 //Get the current time
 const date = new Date();
 //Convert to a readable format
@@ -14,73 +18,57 @@ const formatter = new Intl.DateTimeFormat('en', {
 const post = (type, message) => {
   return new Promise((resolve, reject) => {
 
+    const handleError = (error) => {
+      console.error(`Error while processing log: ${error}`);
+      reject(error); // Pass the error up to be handled by the caller
+    };
+
     switch (type) {
-    case 'success':
-      _toConsole(type, 'green', message)
-        .then(() => {
-          _toFile(type, message)
-            .then(() => {
-              resolve();
-            }, () => {
-              reject('Failed to log to file');
-            });
-        }, () => {
-          reject('Failed to log to console');
-        });
-      break;
-    case 'info':
-      _toConsole(type, 'blue', message)
-        .then(() => {
-          _toFile(type, message)
-            .then(() => {
-              resolve();
-            }, () => {
-              reject('Failed to log to file');
-            });
-        }, () => {
-          reject('Failed to log to console');
-        });
-      break;
-    case 'warn':
-      _toConsole(type, 'yellow', message)
-        .then(() => {
-          _toFile(type, message)
-            .then(() => {
-              resolve();
-            }, () => {
-              reject('Failed to log to file');
-            });
-        }, () => {
-          reject('Failed to log to console');
-        });
-      break;
-    case 'error':
-      _toConsole(type, 'red', message)
-        .then(() => {
-          _toFile(type, message)
-            .then(() => {
-              resolve();
-            }, () => {
-              reject('Failed to log to file');
-            });
-        }, () => {
-          // reject('Failed to log to console');
-          console.log(message);
-        });
-      break;
-    case 'urgent':
-      _toConsole(type, 'bgRed', message)
-        .then(() => {
-          _toFile(type, message)
-            .then(() => {
-              resolve();
-            }, () => {
-              reject('Failed to log to file');
-            });
-        }, () => {
-          reject('Failed to log to console');
-        });
-      break;
+      case 'success':
+        _toConsole(type, 'green', message)
+          .then(() => {
+            _toFile(type, message)
+              .then(resolve)
+              .catch(() => reject('Failed to log to file'));
+          })
+          .catch(handleError);
+        break;
+      case 'info':
+        _toConsole(type, 'blue', message)
+          .then(() => {
+            _toFile(type, message)
+              .then(resolve)
+              .catch(() => reject('Failed to log to file'));
+          })
+          .catch(handleError);
+        break;
+      case 'warn':
+        _toConsole(type, 'yellow', message)
+          .then(() => {
+            _toFile(type, message)
+              .then(resolve)
+              .catch(() => reject('Failed to log to file'));
+          })
+          .catch(handleError);
+        break;
+      case 'error':
+        _toConsole(type, 'red', message)
+          .then(() => {
+            _toFile(type, message)
+              .then(resolve)
+              .catch(() => reject('Failed to log to file'));
+          })
+          .catch(() => console.log(message)); // Log to console if console logging fails
+        break;
+      case 'urgent':
+        _toConsole(type, 'bgRed', message)
+          .then(() => {
+            _toFile(type, message)
+              .then(resolve)
+              .catch(() => reject('Failed to log to file'));
+          })
+          .catch(handleError);
+        break;
     }
   });
 };
@@ -93,7 +81,7 @@ const _toFile = (type, message) => {
   let formattedTime = moment().format('hh:mm:ss');
 
   return new Promise((resolve, reject) => {
-    fs.appendFile(Path.join(__dirname, `../../Data/Logs/${fileTimestamp}`), JSON.stringify({
+    fs.appendFile(path.join(__dirname, `../../Data/Logs/${fileTimestamp}`), JSON.stringify({
       objTime: objectTimestamp,
       time: formattedTime,
       type: type,
@@ -109,16 +97,27 @@ const _toFile = (type, message) => {
 };
 
 //Async write log to console stream
-const _toConsole = (type, colour, message) => {
+const _toConsole = (type, color, message) => {
   return new Promise((resolve, reject) => {
-    //Convert message to array
+    // Convert message to array
     let messageArray = message.split('\n');
 
     try {
-      console.log(chalk `{${colour} ${formatter.format(date)} [${type.toUpperCase()}]} {grey ${messageArray[0]}}`);
-      if(messageArray.length > 1) {
-        console.log(chalk `{${colour} ${formatter.format(date)} [${type.toUpperCase()}]} {grey ${messageArray[1]}}`);
-        console.log(chalk `{${colour} ${formatter.format(date)} [${type.toUpperCase()}]} {grey ${messageArray[2]}}`);
+      // Formatting date and message type
+      const timestamp = formatter.format(new Date()); // Ensure you have a formatter or replace with a suitable formatting.
+      const typeFormatted = `[${type.toUpperCase()}]`;
+
+      // Dynamically selecting the chalk method based on the 'color' parameter
+      const colorize = chalk[color] ? chalk[color] : chalk.white; // Fallback to white if the color is not found
+
+      // Composing and logging messages
+      const logMessage = `${timestamp} ${typeFormatted}`;
+      console.log(colorize(logMessage) + ' ' + chalk.grey(messageArray[0]));
+      if (messageArray.length > 1) {
+        console.log(colorize(logMessage) + ' ' + chalk.grey(messageArray[1]));
+        if (messageArray.length > 2) {
+          console.log(colorize(logMessage) + ' ' + chalk.grey(messageArray[2]));
+        }
       }
       resolve('Log created to console');
     } catch (error) {
@@ -126,6 +125,7 @@ const _toConsole = (type, colour, message) => {
     }
   });
 };
+
 
 export default {
   post
