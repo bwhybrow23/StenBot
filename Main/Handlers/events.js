d3bf48f91fd4242b71811ef7147d2b6f02111814
Ben Whybrow <ben@benwhybrow.com>
2024-05-06 10:36:53 +0100

    Events Handler now working after ES6 migration

--- Main/Handlers/events.js
+++ Main/Handlers/events.js
@@ -1,4 +1,5 @@
-import events from '../../Data/Global/eventNames.js';
+import EventsFile from '../../Data/Global/eventNames.js';
+const EventsNames = EventsFile.Events;
 import { AsciiTable3 } from 'ascii-table3';
 import fs from 'fs/promises';
 
@@ -8,24 +9,25 @@ export async function eventHandler(bot) {
   try {
     const dirs = await fs.readdir('./Main/Events/');
     for (const dir of dirs) {
-      const events = await fs.readdir(`./Main/Events/${dir}/`);
-      for (const file of events) {
+      const eventsFiles = await fs.readdir(`./Main/Events/${dir}/`);
+      for (const file of eventsFiles) {
         if (file.endsWith('.js')) {
-          const event = await import(`../Events/${dir}/${file}`);
-          if (!Events.includes(event.name) || !event.name) {
-            table.addRow(event.name, '❌ -> Missing Something??');
+          const eventFile = await import(`../Events/${dir}/${file}`);
+          const eventObj = eventFile.default;
+          if (!EventsNames.includes(eventObj.name) || !eventObj.name) {
+            table.addRow(eventObj.name, '❌ -> Missing Something??');
           }
-          if (event.once) {
-            bot.once(event.name, (...args) => event.execute(bot, ...args));
+          if (eventObj.once) {
+            bot.once(eventObj.name, (...args) => eventObj.execute(bot, ...args));
           } else {
-            bot.on(event.name, (...args) => event.execute(bot, ...args));
+            bot.on(eventObj.name, (...args) => eventObj.execute(bot, ...args));
           }
-          table.addRow(event.name, '✅');
+          table.addRow(eventObj.name, '✅');
         }
       }
     }
     console.log(table.toString());
   } catch (error) {
-    console.error('Error in eventHandler:', error);
+    bot.log.post('error', error);
   }
 }
