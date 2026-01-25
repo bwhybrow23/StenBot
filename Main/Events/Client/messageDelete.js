2f940723c433366545db9e5c2131332dc4e5da58
Ben Whybrow <ben@benwhybrow.com>
2026-01-25 20:01:37 +0000

    Remove checking for commands on delete

--- Main/Events/Client/messageDelete.js
+++ Main/Events/Client/messageDelete.js
@@ -19,10 +19,6 @@ export default {
       if (config.logging.ignore.includes(message.channel.id)) return;
       if (config.logging.level === 'low' || config.logging.level === 'medium' || config.logging.level === 'high') {
         if (bot.efunctions.checkChannel(config.logging.channel, bot) === true) {
-          let files = await readdirPromisify('./Commands/');
-          let possiblefilename = message.content.split(' ')[0].slice(bot.settings.prefix.length) + '.js';
-          if (files.includes(possiblefilename)) return;
-
           let lchannel = bot.channels.cache.get(config.logging.channel);
           bot.eventEmbed('c70011', message.author, 'Message Deleted', `**Channel:** ${message.channel}\n**Message:**\n${message}`, [], `${lchannel.guild.name}`, bot)
             .then(embed => lchannel.send(embed))
